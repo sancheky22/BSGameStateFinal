9d4980ad10b6cbfbe4e38563a0db6ee4d1fd3e09
magliana22 <magliana22@up.edu>
2019-10-24 09:08:01 -0700

    copied will's toString

--- app/src/main/java/com/example/bsgamestatefinal/BSGameState.java
+++ app/src/main/java/com/example/bsgamestatefinal/BSGameState.java
@@ -126,9 +126,12 @@ public class BSGameState {
 
     @Override
     public String toString() {
-        String gameInfo = new String("");
 
-        return gameInfo; //default return value
+        return "P1 Hits: "+this.p1TotalHits+"P2 Hits: "+this.p2TotalHits
+                +"Player Turn:"+this.playerTurn+"P1 Ships Alive: "+this.p1ShipsAlive +"P2 Ships Alive: "+this.p2ShipsAlive
+                +"P1 Ships Sunk: "+this.p1ShipsSunk+"P2 Ships Sunk: "+this.p2ShipsSunk+"Phase:"+this.phaseOfGame;
+
+
     }
 }
 
