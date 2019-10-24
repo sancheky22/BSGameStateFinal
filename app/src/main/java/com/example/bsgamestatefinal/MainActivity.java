84f46b2288222440f2be49c42c19264a0b00051d
co22 <co22@up.edu>
2019-10-24 09:13:31 -0700

    added string comparison and appended second and fourth instances.

--- app/src/main/java/com/example/bsgamestatefinal/MainActivity.java
+++ app/src/main/java/com/example/bsgamestatefinal/MainActivity.java
@@ -27,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
     /** runTestListener: listener for run test button **/
     private OnClickListener runTestListener = new OnClickListener() {
         public void onClick(View v) {
+            /**
+             External Citation
+             Date: 23 October 2019
+             Problem: Adding a multiline EditText
+             Resource:
+             https://stackoverflow.com/questions/4233626/allow-multi-line-in-edittext-view-in-android
+             https://developer.android.com/reference/android/widget/EditText
+             Solution: I used the example code from this post.
+             */
             multiLineText.setText(""); //clear text from EditText
 
             BSGameState firstInstance = new BSGameState(); //create GameState instance using default constructor
@@ -34,18 +43,25 @@ public class MainActivity extends AppCompatActivity {
 
             //call each method in game state class at least once, print descriptions to multiLineText
             firstInstance.getPlayerTurn();
-            multiLineText.append("Get whether it is player1's turn or player2's turn.\n");
+            multiLineText.append("getPlayerTurn(): Get whether it is player1's turn or player2's turn.\n");
             firstInstance.setPlayerTurn(0);
-            multiLineText.append("Set the current turn to 0 (or player1)");
+            multiLineText.append("setPlayerTurn(): Set the current turn to 0 (or player1)\n");
 
 
             BSGameState thirdInstance = new BSGameState(); //create another GameState instance using default constructor
             BSGameState fourthInstance = new BSGameState(thirdInstance); //create copy of thirdInstance
 
-            secondInstance.toString(); //call toString on secondInstance
-            fourthInstance.toString(); //call toString on fourthInstance
+            String instance2String = secondInstance.toString(); //call toString on secondInstance
+            String instance4String = fourthInstance.toString(); //call toString on fourthInstance
 
             //verify both toString calls are identical, print both strings to multiLineText
+            boolean equals = instance2String.equals(instance4String); //check if instance 2 is the same as instance 4
+            if (equals){
+                multiLineText.append(instance2String +"\n");
+                multiLineText.append(instance4String +"\n");
+            } else{
+                multiLineText.append("Error: instances 2 and 4 do not match.\n");
+            }
 
         }
     };
