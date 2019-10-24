54cf4814b909244bf718731f3d1f745721718bbd
magliana22 <magliana22@up.edu>
2019-10-23 20:14:57 -0700

    Commented out gameState methods

--- app/src/main/java/com/example/bsgamestatefinal/BSGameState.java
+++ app/src/main/java/com/example/bsgamestatefinal/BSGameState.java
@@ -21,19 +21,18 @@ public class BSGameState {
     //private GamePlayer player2;
 
 
-
     public BSGameState() {
-        this.playerTurn=1;
-        this.p1TotalHits=0;
-        this.p2TotalHits=0;
-        this.shipsAlive=10;
-        this.shipsSunk=0;
-        this.isHit=false;
-        this.phaseOfGame="SetUp";
-        this.shotLocations=null;
-        this.shipLocations=null;
-        this.shipType=1;
-        this.board=new String[10][20];
+        this.playerTurn = 1;
+        this.p1TotalHits = 0;
+        this.p2TotalHits = 0;
+        this.shipsAlive = 10;
+        this.shipsSunk = 0;
+        this.isHit = false;
+        this.phaseOfGame = "SetUp";
+        this.shotLocations = null;
+        this.shipLocations = null;
+        this.shipType = 1;
+        this.board = new String[10][20];
         //this.player1=new HumanPlayer;
         //this.player2=new ComputerPlayer;
 
@@ -41,19 +40,18 @@ public class BSGameState {
 
 
     // Copy Constructor
-    public BSGameState(BSGameState original)
-    {
-        this.playerTurn=1;
-        this.p1TotalHits=0;
-        this.p2TotalHits=0;
-        this.shipsAlive=10;
-        this.shipsSunk=0;
-        this.isHit=false;
-        this.phaseOfGame="SetUp";
-        this.shotLocations=null;
-        this.shipLocations=null;
-        this.shipType=1;
-        this.board=new String[10][10];
+    public BSGameState(BSGameState original) {
+        this.playerTurn = 1;
+        this.p1TotalHits = 0;
+        this.p2TotalHits = 0;
+        this.shipsAlive = 10;
+        this.shipsSunk = 0;
+        this.isHit = false;
+        this.phaseOfGame = "SetUp";
+        this.shotLocations = null;
+        this.shipLocations = null;
+        this.shipType = 1;
+        this.board = new String[10][10];
 
         // copy the player-to-move information
 
@@ -62,55 +60,54 @@ public class BSGameState {
     }
 
     /**
-    // Getter for boat selected
-    public int getBoat(int shipType) {
-        if (shipType == 0){
-            return carrier;
-        }else if(shipType == 1){
-            return battleShip;
-        }else if(shipType == 2){
-            return cruiser;
-        }else if(shipType == 3){
-            return submarine;
-        }else if(shipType == 4){
-            return destroyer;
-        }
-    }
-    */
+     * // Getter for boat selected
+     * public int getBoat(int shipType) {
+     * if (shipType == 0){
+     * return carrier;
+     * }else if(shipType == 1){
+     * return battleShip;
+     * }else if(shipType == 2){
+     * return cruiser;
+     * }else if(shipType == 3){
+     * return submarine;
+     * }else if(shipType == 4){
+     * return destroyer;
+     * }
+     * }
+     */
 
     // Setter for boat selected
-    //public void setBoat(Ship shipType, int ship) { return this.shipType; }
-
-    public boolean rotate(int playerID, int shipType, String xCoord, String yCoord, BSGameState gameState){
-        boolean valid=false;
-        if(playerID==gameState.getPlayerTurn()){
-            switch(shipType){
+   /* //public void setBoat(Ship shipType, int ship) { return this.shipType; }
+    public boolean rotate(int playerID, int shipType, String xCoord, String yCoord, BSGameState gameState) {
+        boolean valid = false;
+        if (playerID == gameState.getPlayerTurn()) {
+            switch (shipType) {
                 case 1: //code
-                    valid=true;
+                    valid = true;
                     break;
 
                 case 2: //code
-                    valid=true;
+                    valid = true;
                     break;
 
                 case 3: //code
-                    valid=true;
+                    valid = true;
                     break;
 
                 case 4: //code
-                    valid=true;
+                    valid = true;
                     break;
                 case 5: //code
-                    valid=true;
+                    valid = true;
                     break;
 
                 default: //code
-                    valid=false;
+                    valid = false;
                     break;
             }
         }
         return valid;
-    }
+    }*/
 
     // gets which players turn it is
     public int getPlayerTurn() {
@@ -126,62 +123,64 @@ public class BSGameState {
     /**
      * fire method: take board as parameter & fire on selected coordinates, return true if successful fire
      **/
-    public boolean fire(String[][] bsBoard) {
-        return false; // default return value
-    }
+    //public boolean fire(String[][] bsBoard) {
+    // return false; // default return value
+    //}
 
 
     /**
      * placeShip method: place a ship using given coordinates, return true if ship is placed successfully
      */
-    public boolean placeShip(int playerID, int shipType, String xCoord, String yCoord, BSGameState gameState) {
-        return false; // default return value
-    }
+    //public boolean placeShip(int playerID, int shipType, String xCoord, String yCoord, BSGameState gameState) {
+    //return false; // default return value
+    // }
 
     /**
      * newGame method: to start a new game, return true if successful
      **/
-    public boolean newGame(BSGameState gameState) {
+   /* public boolean newGame(BSGameState gameState) {
         return false; //default return value
     }
 
-    /**
+    *//**
      * quitGame method: to exit the game at any point, return true if successful
-     **/
+     **//*
     public boolean quitGame(BSGameState gameState) {
         return false; //default return value
-    }
+    }*/
 
 
     /**
      * toString method: describes the state of the game as a string
      **/
-    public boolean toString(BSGameState gameState) {
-        return false; //default return value
+    public String toString(BSGameState gameState) {
+        String gameInfo = new String("");
+        return gameInfo; //default return value
     }
+}
 
 
     /**
      * Menu method; implement menu
      **/
-    public boolean implementMenu(BSGameState gameState) {
+ /*   public boolean implementMenu(BSGameState gameState) {
         return false; //default return value
     }
 
 
-    /**
+    *//**
      * Exit game method; exit the game
-     **/
+     **//*
     public boolean exitGame(BSGameState gameState) {
         return false; //default return value
     }
 
 
-    /**
+    *//**
      * CheckIfValidLocation method; check if location is valid, return true if successful
-     **/
+     **//*
     public boolean CheckIfValidLocation(BSGameState gameState) {
         return false; //default return value
     }
 
-}
+}*/
