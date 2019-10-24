d47b35f94ddc5daf30cfba1a6e68870220fc7929
magliana22 <magliana22@up.edu>
2019-10-23 19:48:30 -0700

    Copied over Ship/Location

--- app/src/main/java/com/example/bsgamestatefinal/BSLocation.java
+++ app/src/main/java/com/example/bsgamestatefinal/BSLocation.java
@@ -1,4 +1,33 @@
 package com.example.bsgamestatefinal;
 
 public class BSLocation {
+    private boolean isWater;
+    private boolean isShip;
+    private boolean isHit;
+    private boolean isMiss;
+
+    public BSLocation(){
+        this.isWater=true;
+        this.isShip=false;
+        this.isHit=false;
+        this.isMiss=false;
+    }
+
+    public int checkSpot(BSLocation location){
+        if(location.isWater){
+            return 1;
+        }
+        else if(location.isShip){
+            return 2;
+        }
+        else if(location.isHit){
+            return 3;
+        }
+        else if(location.isMiss){
+            return 4;
+        }
+        else{
+            return 0;
+        }
+    }
 }
--- app/src/main/java/com/example/bsgamestatefinal/BSShip.java
+++ app/src/main/java/com/example/bsgamestatefinal/BSShip.java
@@ -1,4 +1,18 @@
 package com.example.bsgamestatefinal;
 
 public class BSShip {
+    private int shipHealth;
+    private int shipHits;
+    private boolean isSunk;
+    private int xCoord;
+    private int yCoord;
+    private int Owner;
+
+    public BSShip(int xLocation, int yLocation, int idOfOwner, int shipSize){
+        this.xCoord=xLocation;
+        this.yCoord=yLocation;
+        this.Owner=idOfOwner;
+        this.shipHealth=shipSize;
+        this.isSunk=false;
+    }
 }
