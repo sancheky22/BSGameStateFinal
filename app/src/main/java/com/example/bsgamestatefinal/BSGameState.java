df8ee549723caccb1f74b6f9e1b6271a2a3d94b8
William Leung <leungw22@up.edu>
2019-10-25 17:56:33 -0700

    changed checkSpot location to game state and added fire method

--- app/src/main/java/com/example/bsgamestatefinal/BSGameState.java
+++ app/src/main/java/com/example/bsgamestatefinal/BSGameState.java
@@ -10,7 +10,7 @@ public class BSGameState {
 
     public int p1TotalHits;
     public int p2TotalHits;
-    public int playerTurn;
+    public int playerID;
     public int p1ShipsAlive;
     public int p1ShipsSunk;
     public int p2ShipsAlive;
@@ -28,7 +28,7 @@ public class BSGameState {
 
 
     public BSGameState() {
-        this.playerTurn = 1;
+        this.playerID = 1;
         this.p1TotalHits = 0;
         this.p2TotalHits = 0;
         this.p1ShipsAlive = 10;
@@ -48,7 +48,7 @@ public class BSGameState {
 
     // Copy Constructor
     public BSGameState(BSGameState original) {
-        this.playerTurn = 1;
+        this.playerID = 1;
         this.p1TotalHits = 0;
         this.p2TotalHits = 0;
         this.p1ShipsAlive = 10;
@@ -119,25 +119,73 @@ public class BSGameState {
     }*/
 
     // gets which players turn it is
-    public int getPlayerTurn() {
-        return this.playerTurn;
+    public int getPlayerID() {
+        return this.playerID;
     }
 
     // sets current players turn
-    public void setPlayerTurn(int playerTurn) {
-        this.playerTurn = playerTurn;
+    public void setPlayerTurn(int playerID) {
+        this.playerID = playerID;
     }
 
-    public boolean fire(String[][] bsBoard) {
-        return false; // default return value
+    //fire method
+    public boolean fire(int x, int y) {
+
+        if (this.getPlayerID() == 0) {
+            BSLocation temp = this.p1Board[y][x];
+            if(checkSpot(temp)==3 || checkSpot(temp)==4){
+                this.p1Board[y][x] = temp;
+                return false;
+            }
+            else if (checkSpot(temp) == 2) {
+                this.p1TotalHits = this.p1TotalHits + 1;
+                temp.setSpot(3);
+                this.p1Board[y][x] = temp;
+                return true;
+            }
+            else if (checkSpot(temp) == 1){
+               temp.setSpot(4);
+                this.p1Board[y][x] = temp;
+               return false;
+            }
+        }
+        else if (this.getPlayerID() == 1) {
+            BSLocation temp = this.p2Board[y][x];
+            if (checkSpot(temp) == 3 || checkSpot(temp) == 4) {
+                this.p2Board[y][x] = temp;
+                return false;
+            } else if (checkSpot(temp) == 2) {
+                this.p2TotalHits = this.p2TotalHits + 1;
+                temp.setSpot(3);
+                this.p2Board[y][x] = temp;
+                return true;
+            } else if (checkSpot(temp) == 1) {
+                temp.setSpot(4);
+                this.p2Board[y][x] = temp;
+                return false;
+            }
+        }
+    return false;
     }
-    
+    public int checkSpot(BSLocation location) {
+        if (location.isWater) {
+            return 1;
+        } else if (location.isShip) {
+            return 2;
+        } else if (location.isHit) {
+            return 3;
+        } else if (location.isMiss) {
+            return 4;
+        } else {
+            return 0;
+        }
+    }
 
     @Override
     public String toString() {
 
         return "P1 Hits: "+this.p1TotalHits+" P2 Hits: "+this.p2TotalHits
-                +" Player Turn:"+this.playerTurn+" P1 Ships Alive: "+this.p1ShipsAlive +" P2 Ships Alive: "+this.p2ShipsAlive
+                +" Player Turn:"+this.playerID+" P1 Ships Alive: "+this.p1ShipsAlive +" P2 Ships Alive: "+this.p2ShipsAlive
                 +" P1 Ships Sunk: "+this.p1ShipsSunk+" P2 Ships Sunk: "+this.p2ShipsSunk+" Phase:"+this.phaseOfGame;
 
 
@@ -145,24 +193,22 @@ public class BSGameState {
 }
 
 
-    /**
-     * fire method: take board as parameter & fire on selected coordinates, return true if successful fire
-     **/
-    //public boolean fire(String[][] bsBoard) {
-    // return false; // default return value
+
+     /** fire method: take board as parameter & fire on selected coordinates, return true if successful fire
+
+      public boolean fire(String[][] bsBoard) {
+    return false; // default return value
     //}
 
 
-    /**
-     * placeShip method: place a ship using given coordinates, return true if ship is placed successfully
-     */
-    //public boolean placeShip(int playerID, int shipType, String xCoord, String yCoord, BSGameState gameState) {
+    // * placeShip method: place a ship using given coordinates, return true if ship is placed successfully
+
+    public boolean placeShip(int playerID, int shipType, String xCoord, String yCoord, BSGameState gameState) {
     //return false; // default return value
     // }
 
-    /**
-     * newGame method: to start a new game, return true if successful
-     **/
+     //* newGame method: to start a new game, return true if successful
+     //*
    /* public boolean newGame(BSGameState gameState) {
         return false; //default return value
     }
@@ -173,12 +219,6 @@ public class BSGameState {
     public boolean quitGame(BSGameState gameState) {
         return false; //default return value
     }*/
-
-
-    /**
-     * toString method: describes the state of the game as a string
-     **/
-
 
 
     /**
--- app/src/main/java/com/example/bsgamestatefinal/BSLocation.java
+++ app/src/main/java/com/example/bsgamestatefinal/BSLocation.java
@@ -7,10 +7,10 @@ package com.example.bsgamestatefinal;
  * **/
 
 public class BSLocation {
-    private boolean isWater;
-    private boolean isShip;
-    private boolean isHit;
-    private boolean isMiss;
+    public boolean isWater;
+    public boolean isShip;
+    public boolean isHit;
+    public boolean isMiss;
 
     public BSLocation(){
         this.isWater=true;
@@ -19,21 +19,4 @@ public class BSLocation {
         this.isMiss=false;
     }
 
-    public int checkSpot(BSLocation location){
-        if(location.isWater){
-            return 1;
-        }
-        else if(location.isShip){
-            return 2;
-        }
-        else if(location.isHit){
-            return 3;
-        }
-        else if(location.isMiss){
-            return 4;
-        }
-        else{
-            return 0;
-        }
-    }
 }
